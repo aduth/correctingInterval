fc2a0b966a8c7796ff06653f4f9bc4a70101e43e
Andrew Duthie <aduthie7@gmail.com>
2013-12-16 19:55:14 -0500

    Apply consistent tabbing to gruntfile
    2 space tabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,53 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        banner: '/*! <%= pkg.name %> <%= pkg.version %> | Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> | <%= pkg.license %> License */\n',
+    banner: '/*! <%= pkg.name %> <%= pkg.version %> | Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> | <%= pkg.license %> License */\n',
 
-        concat: {
-            options: {
-                stripBanners: { block: true },
-                banner: '<%= banner %>'
-            },
-            dist: {
-                src: [ 'correctingInterval.js' ],
-                dest: 'correctingInterval.js'
-            }
+    concat: {
+      options: {
+        stripBanners: {
+          block: true
         },
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: [ 'correctingInterval.js' ],
+        dest: 'correctingInterval.js'
+      }
+    },
 
-        uglify: {
-            options: {
-                banner: '<%= banner %>'
-            },
-            dist: {
-                files: {
-                    'correctingInterval.min.js': [ 'correctingInterval.js' ]
-                }
-            }
-        },
-
-        jshint: {
-            files: [
-                'correctingInterval.js'
-            ]
-        },
-
-        mocha: {
-            index: ['test/index.html'],
-            options: {
-                run: true
-            }
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        files: {
+          'correctingInterval.min.js': [ 'correctingInterval.js' ]
         }
-    });
+      }
+    },
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-mocha');
+    jshint: {
+      files: [
+        'correctingInterval.js'
+      ]
+    },
 
-    grunt.registerTask('test', [ 'jshint', 'mocha' ]);
-    grunt.registerTask('compile', [ 'test', 'concat', 'uglify' ]);
-    grunt.registerTask('default', [ 'compile' ]);
+    mocha: {
+      index: ['test/index.html'],
+      options: {
+        run: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha');
+
+  grunt.registerTask('test', [ 'jshint', 'mocha' ]);
+  grunt.registerTask('compile', [ 'test', 'concat', 'uglify' ]);
+  grunt.registerTask('default', [ 'compile' ]);
 };
