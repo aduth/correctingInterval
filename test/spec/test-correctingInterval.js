28dc2bacd774c58660cd210f475f1f2d8d0720e6
Andrew Duthie <aduthie7@gmail.com>
2013-12-16 20:41:21 -0500

    Add some real tests!

--- test/spec/test-correctingInterval.js
+++ test/spec/test-correctingInterval.js
@@ -1,11 +1,83 @@
 describe('setCorrectingInterval', function() {
+  var noop = function() { };
+
   it('should be defined', function() {
     expect(setCorrectingInterval).to.be.ok;
+  });
+
+  it('should return a unique id', function() {
+  	var iter = 5,
+      defined = {};
+
+    for (var i = 0; i < iter; i++) {
+      var intervalId = setCorrectingInterval(noop, 1000);
+      defined[intervalId] = true;
+      clearCorrectingInterval(intervalId);
+    }
+
+    expect(Object.keys(defined).length).to.equal(iter);
+  });
+
+  it('should execute callback after defined delay', function(done) {
+    var delay = 500,
+      now = Date.now(),
+      intervalId;
+
+    intervalId = setCorrectingInterval(function() {
+      console.log('abc')
+      expect(Math.abs(Date.now() - now - delay)).to.be.within(0, 25);
+      clearCorrectingInterval(intervalId);
+      done();
+    }, delay);
+  });
+
+  it('should continuously execute callback after defined delay', function(done) {
+    var delay = 100,
+      iter = 0,
+      intervalId;
+
+    intervalId = setCorrectingInterval(function() {
+      console.log('abc')
+      if (++iter === 2) {
+        clearCorrectingInterval(intervalId);
+        done();
+      }
+    }, delay);
+  });
+
+  it('should be relatively accurate', function(done) {
+    var delay = target = 200,
+      previousDelay = undefined,
+      now = Date.now();
+
+    setCorrectingInterval(function() {
+      // Expect at least one interval to be more accurate than its
+      //  previous before timeout occurs
+
+      var thisDelay = Date.now() - now - target;
+      if (previousDelay !== 'undefined' && thisDelay < previousDelay) done();
+      previousDelay = thisDelay;
+      target += delay;
+    }, delay)
   });
 });
 
 describe('clearCorrectingInterval', function() {
   it('should be defined', function() {
     expect(clearCorrectingInterval).to.be.ok;
+  });
+
+  it('should clear a running interval', function(done) {
+    var iter = 0,
+      intervalId;
+
+    intervalId = setCorrectingInterval(function() {
+      if (++iter === 2) throw new Error('expected to clear interval');
+      clearCorrectingInterval(intervalId);
+    }, 50);
+
+    setTimeout(function() {
+      done();
+    }, 200);
   });
 });
