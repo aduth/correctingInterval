d26c4eb8b229a35b2125c91e46e2726006a1756e
dan <mybodya@gmail.com>
2014-05-05 15:56:26 +0200

    removed date.now() polyfill. removed support for passing code as string to evaluate. cleaning up code for maximum performance. Fixing bug with Object prototype pollution. Fixing a memory leak. Fixing unreadable code

--- correctingInterval.js
+++ correctingInterval.js
@@ -1,81 +1,45 @@
-/*! correcting-interval 1.0.3 | Copyright 2014 Andrew Duthie | MIT License */
-/* jshint evil: true */
-
 ;(function(global, factory) {
-  // Use UMD pattern to expose exported functions
-  if (typeof exports === 'object') {
-    // Expose to Node.js
-    module.exports = factory();
-  } else if (typeof define === 'function' && define.amd) {
-    // Expose to RequireJS
-    define([], factory);
-  }
+    // Use UMD pattern to expose exported functions
+    if (typeof exports === 'object') {
+        // Expose to Node.js
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // Expose to RequireJS
+        define([], factory);
+    }
 
-  // Expose to global object (likely browser window)
-  var exports = factory();
-  for (var prop in exports) {
-    global[prop] = exports[prop];
-  }
+    // Expose to global object (likely browser window)
+    var exports = factory();
+    for (var prop in exports) {
+        if (exports.hasOwnProperty(prop)) {
+            global[prop] = exports[prop];
+        }
+    }
 }(this, function() {
-  // Track running intervals
-  var correctingIntervals = [];
 
-  // Polyfill Date.now
-  var now = Date.now || function() {
-    return new Date().valueOf();
-  };
+    var autoIncrement = 0,
+        intervals = {};
 
-  var setCorrectingInterval = function() {
-    // Create closure to save instance settings
-    var instance = { };
+    return {
+        setCorrectingInterval: function (func, delay, minDelay /* optional */) {
+            var id = autoIncrement++,
+                planned = Date.now() + delay;
 
-    // Save to global running intervals
-    instance.guid = correctingIntervals.length;
-    correctingIntervals.push(instance);
+            minDelay = minDelay || 0;
 
-    var tick = function(func, delay) {
-      if (!instance.started) {
-        // On first call, save instance settings
-        if (typeof func === 'string') {
-          // Convert string to function
-          func = function() { eval(this.func); }.bind({ func: func });
+            function tick () {
+                func();
+                planned += delay;
+                intervals[id] = setTimeout(tick, Math.max(planned - Date.now(), minDelay));
+            }
+
+            intervals[id] = setTimeout(tick, delay);
+            return id;
+        },
+
+        clearCorrectingInterval: function (id) {
+            clearTimeout(intervals[id]);
+            delete intervals[id];
         }
-        instance.func = func instanceof Function ? func : (function() { });
-        instance.delay = delay;
-        instance.startTime = now();
-        instance.target = delay;
-        instance.started = true;
-        instance.intervalId = setTimeout.apply(this, [ tick ].concat([].slice.call(arguments, 1)));
-        return instance.guid;
-      } else {
-        // On subsequent iterations, adjust timeout to correct for latency
-        var elapsed = now() - instance.startTime,
-          adjust = instance.target - elapsed,
-          args = [].slice.call(arguments),
-          timeoutArgs = [ tick, instance.delay + adjust ].concat(args);
-
-        instance.func.apply(this, args);
-        if (correctingIntervals[instance.guid]) {
-          // Only re-register if clearCorrectingInterval was not called during function
-          instance.target += instance.delay;
-          instance.intervalId = setTimeout.apply(this, timeoutArgs);
-        }
-      }
     };
-
-    return tick.apply(this, [].slice.call(arguments));
-  };
-
-  var clearCorrectingInterval = function(intervalId) {
-    if (!correctingIntervals[intervalId]) return;
-
-    // Clear existing timeout and remove from global running intervals
-    clearTimeout(correctingIntervals[intervalId].intervalId);
-    correctingIntervals[intervalId] = null;
-  };
-
-  return {
-    setCorrectingInterval: setCorrectingInterval,
-    clearCorrectingInterval: clearCorrectingInterval
-  };
 }));
