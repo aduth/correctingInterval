f80069c10eba2f3eacb6adcbe70e974f02a8bed1
dan <mybodya@gmail.com>
2014-05-06 17:15:43 +0200

    fixing bug: interval doe nor clear from  funciton

--- correctingInterval.js
+++ correctingInterval.js
@@ -1,45 +1,27 @@
-;(function(global, factory) {
-    // Use UMD pattern to expose exported functions
-    if (typeof exports === 'object') {
-        // Expose to Node.js
-        module.exports = factory();
-    } else if (typeof define === 'function' && define.amd) {
-        // Expose to RequireJS
-        define([], factory);
-    }
-
-    // Expose to global object (likely browser window)
-    var exports = factory();
-    for (var prop in exports) {
-        if (exports.hasOwnProperty(prop)) {
-            global[prop] = exports[prop];
-        }
-    }
-}(this, function() {
-
+(function() {
     var autoIncrement = 0,
         intervals = {};
 
-    return {
-        setCorrectingInterval: function (func, delay, minDelay /* optional */) {
-            var id = autoIncrement++,
-                planned = Date.now() + delay;
+    window.setCorrectingInterval = function (func, delay, minDelay /* optional */) {
+        var id = autoIncrement++,
+            planned = Date.now() + delay;
 
-            minDelay = minDelay || 0;
+        minDelay = minDelay || 0;
 
-            function tick () {
-                func();
+        function tick () {
+            func();
+            if (intervals[id]) {
                 planned += delay;
                 intervals[id] = setTimeout(tick, Math.max(planned - Date.now(), minDelay));
             }
-
-            intervals[id] = setTimeout(tick, delay);
-            return id;
-        },
-
-        clearCorrectingInterval: function (id) {
-            clearTimeout(intervals[id]);
-            delete intervals[id];
         }
+
+        intervals[id] = setTimeout(tick, delay);
+        return id;
     };
-}));
+
+    window.clearCorrectingInterval = function (id) {
+        clearTimeout(intervals[id]);
+        delete intervals[id];
+    };
+})();
