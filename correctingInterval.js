cebe4826d6da576f66d4411e1013c4f64d98ff7b
Andrew Duthie <aduthie7@gmail.com>
2014-05-17 12:04:30 -0400

    Resolve tabbing inconsistency (use two spaces)

--- correctingInterval.js
+++ correctingInterval.js
@@ -1,27 +1,27 @@
 (function() {
-    var autoIncrement = 0,
-        intervals = {};
+  var autoIncrement = 0,
+    intervals = {};
 
-    window.setCorrectingInterval = function (func, delay, minDelay /* optional */) {
-        var id = autoIncrement++,
-            planned = Date.now() + delay;
+  window.setCorrectingInterval = function (func, delay, minDelay /* optional */) {
+    var id = autoIncrement++,
+      planned = Date.now() + delay;
 
-        minDelay = minDelay || 0;
+    minDelay = minDelay || 0;
 
-        function tick () {
-            func();
-            if (intervals[id]) {
-                planned += delay;
-                intervals[id] = setTimeout(tick, Math.max(planned - Date.now(), minDelay));
-            }
-        }
+    function tick () {
+      func();
+      if (intervals[id]) {
+        planned += delay;
+        intervals[id] = setTimeout(tick, Math.max(planned - Date.now(), minDelay));
+      }
+    }
 
-        intervals[id] = setTimeout(tick, delay);
-        return id;
-    };
+    intervals[id] = setTimeout(tick, delay);
+    return id;
+  };
 
-    window.clearCorrectingInterval = function (id) {
-        clearTimeout(intervals[id]);
-        delete intervals[id];
-    };
+  window.clearCorrectingInterval = function (id) {
+    clearTimeout(intervals[id]);
+    delete intervals[id];
+  };
 })();
